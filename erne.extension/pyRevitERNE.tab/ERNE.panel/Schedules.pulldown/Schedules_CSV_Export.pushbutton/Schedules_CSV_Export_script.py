a880db1db57b0a4a565f16f21db17e576b6f1cf6
fbpyr <frederic.beaupere@erne.net>
2021-06-04 14:53:56 +0200

    add export schedules

---
+++ erne.extension/pyRevitERNE.tab/ERNE.panel/Schedules.pulldown/Schedules_CSV_Export.pushbutton/Schedules_CSV_Export_script.py
@@ -0,0 +1,57 @@
+# -*- coding: cp1252 -*-
+import clr
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import ViewScheduleExportOptions, ExportTextQualifier, ExportColumnHeaders
+from rpw.ui.forms import select_folder
+from rpw import doc, uidoc
+import datetime
+
+
+def get_sched_raw_options():
+    sched_opt = ViewScheduleExportOptions()
+    sched_opt.Title = False
+    sched_opt.FieldDelimiter = ";"
+    sched_opt.HeadersFootersBlanks = False
+    sched_opt.TextQualifier = ExportTextQualifier.None
+    ViewScheduleExportOptions.ColumnHeaders.SetValue(
+        sched_opt,
+        ExportColumnHeaders.None,
+    )
+    return sched_opt
+
+
+def get_sched_formatted_options():
+    sched_opt = ViewScheduleExportOptions()
+    sched_opt.Title = True
+    sched_opt.FieldDelimiter = ";"
+    sched_opt.HeadersFootersBlanks = True
+    sched_opt.TextQualifier = ExportTextQualifier.None
+    ViewScheduleExportOptions.ColumnHeaders.SetValue(
+        sched_opt,
+        ExportColumnHeaders.MultipleRows,
+    )
+    return sched_opt
+
+
+export_dir = select_folder()
+export_modes = {
+    "raw"      : get_sched_raw_options(),
+    "formatted": get_sched_formatted_options(),
+}
+
+selection = [doc.GetElement(elId) for elId in uidoc.Selection.GetElementIds()]
+today_date = str(datetime.datetime.now().date()).replace("-","")
+
+for view in selection:
+    if str(view.ViewType) != 'Schedule':
+        continue
+
+    sched_name = view.Name.replace(" ", "_")
+    print("exporting: {}".format(sched_name))
+
+    for mode, sched_opt in export_modes.items():
+        view.Export(
+            export_dir,
+            "{}_{}_{}.csv".format(today_date, sched_name, mode),
+            sched_opt,
+        )
